2e7fe1b110d5b250d7171749d918251854a24930
s <oridelpa@gmail.com>
2026-03-18 01:50:36 -0300

    Este codigo se uso para exportar las bases

---
+++ Exportacion para POWERBI.sql
@@ -0,0 +1,35 @@
+SELECT * FROM marcas;
+
+SELECT * FROM categorias; 
+
+SELECT * FROM talles;
+
+SELECT * FROM temporadas; 
+
+SELECT * FROM estado_prenda;
+
+SELECT * FROM metodos_pago; 
+
+SELECT * FROM sucursales;
+
+SELECT * FROM estados_pago;
+
+SELECT * FROM clientes;
+
+SELECT * FROM proveedores;
+
+SELECT * FROM productos;
+
+SELECT * FROM ventas;
+
+SELECT * FROM detalle_ventas;
+
+SELECT * FROM pagos_proveedores;
+
+SELECT * FROM promociones;
+
+SELECT * FROM log_cambios_precios;
+
+SELECT * FROM vista_pagos_proveedores_resumen;
+
+SELECT * FROM vista_ranking_clientes;
